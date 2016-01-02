b8b96e36cd99f1bcef167a635b6d263c99733ac4
deansmylie1993 <k1423253@kingston.ac.uk>
2016-01-02 09:59:56 +0000

    Digital Clock

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblGMTtime.Text = DateTime.Now.ToString("HH:mm:ss");
+            DateTime thisDay = DateTime.Today;
+            label1.Text = thisDay.ToString("d");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
